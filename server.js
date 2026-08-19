d96297011d1423056dc4ead8a5996c110361ff75
Henry <travelapproject@gmail.com>
2026-08-19 13:56:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(process.env.UPLOAD_DIR || path.join(__dirname, 'uploads')));
 
-app.use('/api/tables', require('./routes/tables'));
-app.use('/api/posts', require('./routes/posts'));
-app.use('/api/upload', require('./routes/upload'));
+app.use('/api/tables', require('./tables'));
+app.use('/api/posts', require('./posts'));
+app.use('/api/upload', require('./upload'));
 
 app.get('/', (req, res) => res.send('Friends Fried API running'));
 
